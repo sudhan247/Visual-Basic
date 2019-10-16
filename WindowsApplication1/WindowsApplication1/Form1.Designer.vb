8f2fc5df276c68804ca380b6741ff1b184023b8b
sudhan247 <56646285+sudhan247@users.noreply.github.com>
2019-10-16 21:22:39 +0530

    Add files via upload

---
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,349 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FILEToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.INSERTToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VIEWToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.a = New System.Windows.Forms.TextBox()
+        Me.b = New System.Windows.Forms.TextBox()
+        Me.c = New System.Windows.Forms.TextBox()
+        Me.d = New System.Windows.Forms.TextBox()
+        Me.ee = New System.Windows.Forms.TextBox()
+        Me.f = New System.Windows.Forms.TextBox()
+        Me.g = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.HariBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.StudentDataSet = New WindowsApplication1.studentDataSet()
+        Me.z = New System.Windows.Forms.ComboBox()
+        Me.HariTableAdapter = New WindowsApplication1.studentDataSetTableAdapters.hariTableAdapter()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.HariBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.StudentDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FILEToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1338, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FILEToolStripMenuItem
+        '
+        Me.FILEToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.INSERTToolStripMenuItem, Me.VIEWToolStripMenuItem})
+        Me.FILEToolStripMenuItem.ForeColor = System.Drawing.Color.Red
+        Me.FILEToolStripMenuItem.Name = "FILEToolStripMenuItem"
+        Me.FILEToolStripMenuItem.Size = New System.Drawing.Size(47, 24)
+        Me.FILEToolStripMenuItem.Text = "FILE"
+        '
+        'INSERTToolStripMenuItem
+        '
+        Me.INSERTToolStripMenuItem.ForeColor = System.Drawing.Color.Blue
+        Me.INSERTToolStripMenuItem.Name = "INSERTToolStripMenuItem"
+        Me.INSERTToolStripMenuItem.Size = New System.Drawing.Size(131, 26)
+        Me.INSERTToolStripMenuItem.Text = "INSERT"
+        '
+        'VIEWToolStripMenuItem
+        '
+        Me.VIEWToolStripMenuItem.ForeColor = System.Drawing.Color.Red
+        Me.VIEWToolStripMenuItem.Name = "VIEWToolStripMenuItem"
+        Me.VIEWToolStripMenuItem.Size = New System.Drawing.Size(131, 26)
+        Me.VIEWToolStripMenuItem.Text = "VIEW"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Monotype Corsiva", 13.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(207, 94)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(164, 28)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "REGISTER NO"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Monotype Corsiva", 13.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(210, 149)
+        Me.Label2.Name = "Label2"
+        Me.Label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.Label2.Size = New System.Drawing.Size(83, 28)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "NAME"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Monotype Corsiva", 13.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Red
+        Me.Label3.Location = New System.Drawing.Point(210, 213)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 28)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "AGE"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Monotype Corsiva", 13.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Blue
+        Me.Label4.Location = New System.Drawing.Point(207, 272)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(119, 28)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "ADDRESS"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Monotype Corsiva", 13.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Red
+        Me.Label5.Location = New System.Drawing.Point(207, 332)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(62, 28)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "CITY"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Monotype Corsiva", 13.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.Blue
+        Me.Label6.Location = New System.Drawing.Point(207, 393)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(53, 28)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "JOB"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Monotype Corsiva", 13.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.Red
+        Me.Label7.Location = New System.Drawing.Point(207, 459)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(92, 28)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "PHONE"
+        '
+        'a
+        '
+        Me.a.Font = New System.Drawing.Font("Monotype Corsiva", 13.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.a.Location = New System.Drawing.Point(452, 91)
+        Me.a.Name = "a"
+        Me.a.Size = New System.Drawing.Size(394, 33)
+        Me.a.TabIndex = 8
+        Me.a.Visible = False
+        '
+        'b
+        '
+        Me.b.Font = New System.Drawing.Font("Monotype Corsiva", 13.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.b.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.b.Location = New System.Drawing.Point(452, 144)
+        Me.b.Name = "b"
+        Me.b.Size = New System.Drawing.Size(394, 33)
+        Me.b.TabIndex = 9
+        '
+        'c
+        '
+        Me.c.Font = New System.Drawing.Font("Monotype Corsiva", 13.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.c.ForeColor = System.Drawing.Color.Red
+        Me.c.Location = New System.Drawing.Point(452, 208)
+        Me.c.Name = "c"
+        Me.c.Size = New System.Drawing.Size(394, 33)
+        Me.c.TabIndex = 10
+        '
+        'd
+        '
+        Me.d.Font = New System.Drawing.Font("Monotype Corsiva", 13.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.d.ForeColor = System.Drawing.Color.Blue
+        Me.d.Location = New System.Drawing.Point(452, 267)
+        Me.d.Name = "d"
+        Me.d.Size = New System.Drawing.Size(394, 33)
+        Me.d.TabIndex = 11
+        '
+        'ee
+        '
+        Me.ee.Font = New System.Drawing.Font("Monotype Corsiva", 13.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ee.ForeColor = System.Drawing.Color.Red
+        Me.ee.Location = New System.Drawing.Point(452, 327)
+        Me.ee.Name = "ee"
+        Me.ee.Size = New System.Drawing.Size(394, 33)
+        Me.ee.TabIndex = 12
+        '
+        'f
+        '
+        Me.f.Font = New System.Drawing.Font("Monotype Corsiva", 13.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.f.ForeColor = System.Drawing.Color.Blue
+        Me.f.Location = New System.Drawing.Point(452, 388)
+        Me.f.Name = "f"
+        Me.f.Size = New System.Drawing.Size(394, 33)
+        Me.f.TabIndex = 13
+        '
+        'g
+        '
+        Me.g.Font = New System.Drawing.Font("Monotype Corsiva", 13.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.g.ForeColor = System.Drawing.Color.Red
+        Me.g.Location = New System.Drawing.Point(452, 454)
+        Me.g.Name = "g"
+        Me.g.Size = New System.Drawing.Size(394, 33)
+        Me.g.TabIndex = 14
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Enabled = False
+        Me.DataGridView1.Location = New System.Drawing.Point(86, 507)
+        Me.DataGridView1.MultiSelect = False
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(1007, 434)
+        Me.DataGridView1.TabIndex = 15
+        '
+        'HariBindingSource
+        '
+        Me.HariBindingSource.DataMember = "hari"
+        Me.HariBindingSource.DataSource = Me.StudentDataSet
+        '
+        'StudentDataSet
+        '
+        Me.StudentDataSet.DataSetName = "studentDataSet"
+        Me.StudentDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'z
+        '
+        Me.z.Font = New System.Drawing.Font("Monotype Corsiva", 13.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.z.FormattingEnabled = True
+        Me.z.Location = New System.Drawing.Point(452, 91)
+        Me.z.Name = "z"
+        Me.z.Size = New System.Drawing.Size(394, 35)
+        Me.z.TabIndex = 16
+        Me.z.Visible = False
+        '
+        'HariTableAdapter
+        '
+        Me.HariTableAdapter.ClearBeforeFill = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Monotype Corsiva", 13.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(1074, 258)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(128, 42)
+        Me.Button1.TabIndex = 17
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Monotype Corsiva", 13.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(1074, 332)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(128, 42)
+        Me.Button2.TabIndex = 18
+        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Info
+        Me.ClientSize = New System.Drawing.Size(1338, 1026)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.z)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.g)
+        Me.Controls.Add(Me.f)
+        Me.Controls.Add(Me.ee)
+        Me.Controls.Add(Me.d)
+        Me.Controls.Add(Me.c)
+        Me.Controls.Add(Me.b)
+        Me.Controls.Add(Me.a)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.HariBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.StudentDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FILEToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents INSERTToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents VIEWToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents a As TextBox
+    Friend WithEvents b As TextBox
+    Friend WithEvents c As TextBox
+    Friend WithEvents d As TextBox
+    Friend WithEvents ee As TextBox
+    Friend WithEvents f As TextBox
+    Friend WithEvents g As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents z As ComboBox
+    Friend WithEvents StudentDataSet As studentDataSet
+    Friend WithEvents HariBindingSource As BindingSource
+    Friend WithEvents HariTableAdapter As studentDataSetTableAdapters.hariTableAdapter
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,173 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Dim con As SqlConnection
+    Dim com As SqlCommand
+    Dim rd As SqlDataReader
+
+    Private Sub INSERTToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles INSERTToolStripMenuItem.Click
+        a.Visible = True
+        z.Visible = False
+        a.Clear()
+        b.Clear()
+        c.Clear()
+        d.Clear()
+        ee.Clear()
+        f.Clear()
+        g.Clear()
+        Button1.Text = "SAVE"
+        Button1.BackColor = Color.Green
+
+    End Sub
+    Private Sub SetupDGVData()
+        With DataGridView1
+
+            .Columns.Add("regno", "regno")
+            .Columns.Add("name", "name")
+            .Columns.Add("age", "age")
+            .Columns.Add("address", "address")
+            .Columns.Add("city", "city")
+            .Columns.Add("job", "job")
+            .Columns.Add("phone", "phone")
+
+        End With
+    End Sub
+    Private Sub RetrieveData(Optional ByVal blnSearch As Boolean = False)
+        com.CommandText = "select * from hari"
+        rd = com.ExecuteReader
+        DataGridView1.Rows.Clear()
+        Dim i As Long = DataGridView1.RowCount
+            While rd.Read
+                With DataGridView1
+                .Rows.Add(i)
+                .Rows(i).Cells(0).Value = rd.GetString(0)
+                .Rows(i).Cells(1).Value = rd.GetString(1)
+                .Rows(i).Cells(2).Value = rd.GetString(2)
+                .Rows(i).Cells(3).Value = rd.GetString(3)
+                .Rows(i).Cells(4).Value = rd.GetString(4)
+                .Rows(i).Cells(5).Value = rd.GetString(5)
+                .Rows(i).Cells(6).Value = rd.GetString(6)
+            End With
+            i += 1
+        End While
+        rd.Close()
+    End Sub
+
+    Private Sub VIEWToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles VIEWToolStripMenuItem.Click
+        z.Visible = True
+        a.Visible = False
+        a.Clear()
+        b.Clear()
+        c.Clear()
+        d.Clear()
+        ee.Clear()
+        f.Clear()
+        g.Clear()
+        Button2.Visible = True
+
+        Button1.Text = "UPDATE"
+        Button2.Text = "DELETE"
+        Button2.BackColor = Color.Red
+        DataGridView1.Enabled = True
+        Button1.BackColor = Color.Blue
+        Call sudhan()
+    End Sub
+
+    Private Sub sudhan()
+        com.CommandText = "select * from hari"
+        rd = com.ExecuteReader
+        z.Items.Clear()
+        While rd.Read
+            z.Items.Add(rd.GetString(0))
+        End While
+
+
+
+        rd.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        con = New SqlConnection
+        com = New SqlCommand
+        con.ConnectionString = "Data Source=.\SQLEXPRESS;Initial Catalog=student;Integrated Security=True"
+        con.Open()
+        com.Connection = con
+        z.Items.Clear()
+        Call SetupDGVData()
+        Call RetrieveData()
+    End Sub
+
+    Private Sub z_SelectedIndexChanged(sender As Object, e As EventArgs) Handles z.SelectedIndexChanged
+        com.CommandText = "select * from hari where regno='" & z.SelectedItem & "'"
+        com.ExecuteNonQuery()
+
+        Call deepak()
+
+    End Sub
+    Private Sub deepak()
+        rd = com.ExecuteReader
+
+        If rd.Read Then
+            b.Text = rd.GetString(1)
+            c.Text = rd.GetString(2)
+            d.Text = rd.GetString(3)
+            ee.Text = rd.GetString(4)
+            f.Text = rd.GetString(5)
+            g.Text = rd.GetString(6)
+        End If
+        rd.Close()
+    End Sub
+
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Button1.Text = "SAVE" Then
+
+            If a.Text = "" Or b.Text = "" Or c.Text = "" Or d.Text = "" Or ee.Text = "" Or f.Text = "" Or g.Text = "" Then
+                MsgBox("PLEASE FILL ALL THE DETAILS")
+                Exit Sub
+
+            End If
+            com.CommandText = "insert into hari values('" & a.Text & "','" & b.Text & "','" & c.Text & "','" & d.Text & "','" & ee.Text & "','" & f.Text & "','" & g.Text & "')"
+            com.ExecuteNonQuery()
+            MsgBox("SUCCESS")
+            z.Items.Clear()
+            Call RetrieveData()
+            Call sudhan()
+
+        Else
+            com.CommandText = "update hari set name='" & b.Text & "',age='" & c.Text & "',address='" & d.Text & "',city='" & ee.Text & "',job='" & f.Text & "',phone='" & g.Text & "'where regno='" & z.SelectedItem & "'"
+            com.ExecuteNonQuery()
+            MsgBox("UPDATED")
+            Call RetrieveData()
+        End If
+    End Sub
+
+    Private Sub DataGridView1_DoubleClick(sender As Object, e As EventArgs) Handles DataGridView1.DoubleClick
+        Dim j As Integer
+        j = DataGridView1.CurrentRow.Index
+        z.Text = DataGridView1.Item(0, j).Value
+        b.Text = DataGridView1.Item(1, j).Value
+        c.Text = DataGridView1.Item(2, j).Value
+        d.Text = DataGridView1.Item(3, j).Value
+        ee.Text = DataGridView1.Item(4, j).Value
+        f.Text = DataGridView1.Item(5, j).Value
+        g.Text = DataGridView1.Item(6, j).Value
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        com.CommandText = "delete from hari where regno='" & z.SelectedItem & "'"
+        com.ExecuteNonQuery()
+        MsgBox("RECORD DELETED")
+        z.Items.Remove(z.SelectedItem)
+        a.Clear()
+        b.Clear()
+        c.Clear()
+        d.Clear()
+        ee.Clear()
+        f.Clear()
+        g.Clear()
+        Call RetrieveData()
+    End Sub
+End Class
